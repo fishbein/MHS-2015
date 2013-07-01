e74cc267083ad59b02e8f25148ad7dc1a36a338d
Phil Fishbein <phil@fishbe.in>
2013-07-01 10:36:27 -0400

    Testing

--- views/header.php
+++ views/header.php
@@ -1,2 +1,24 @@
 <?php require('../config.php'); ?>
-Hi.
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Marlboro High School Class of 2015</title>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="../css/foundation.css" />
+  
+
+  <script src="../js/vendor/custom.modernizr.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('h1').delay('100').fadeIn('slow');
+      $('p.one').delay('600').fadeIn('slow');
+      $('p.two').delay('1200').fadeIn('slow');
+  });
+    
+  </script>
+</head>

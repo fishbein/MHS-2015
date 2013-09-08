087b0b5b942918790dd33e01c9ee28be8d916897
Phil Fishbein <phil@fishbe.in>
2013-09-08 19:15:47 -0400

    Fixing merge

--- start.php
+++ start.php
@@ -1,15 +1,5 @@
 <?php
 session_start();
 require("config.php");
-<<<<<<< HEAD
 require("class.php");
-
-if($_SESSION['admin']=="admin"){$admin=TRUE;}
-=======
-//require("class.php");
->>>>>>> master
-
-//Database Connection
-$con = mysql_connect(DB_HOSTNAME,DB_USER,DB_PASSWORD);
-$db = mysql_select_db(DB_NAME);
 ?>
